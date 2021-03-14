0c1cba2300fe73f91f4516688f3b9b7b5af451a5
33cun <xiao_0013@163.com>
2021-03-14 23:59:06 +0800

    * 第 70 题 爬楼梯 1. 斐波那契数列 O(n)

---
+++ src/com/eleven/Q70_爬楼梯/A1/Solution.java
@@ -0,0 +1,56 @@
+package com.eleven.Q70_爬楼梯.A1;
+
+/**
+ * 70. 爬楼梯
+ * 难度
+ * 简单
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ */
+
+
+/** 解法1
+ * 时间复杂度 O(n)
+ *
+ * 该题实则为求解斐波那契数列
+ * f(n) = f(n - 1) + f(n - 2)
+ */
+
+public class Solution {
+    public static int climbStairs(int n) {
+        if (n <= 3) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));;
+    }
+}
