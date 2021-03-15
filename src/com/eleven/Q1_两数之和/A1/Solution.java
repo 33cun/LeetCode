81b1cc6af9850eefa46e031c5df6355b70b3e6cf
33cun <xiao_0013@163.com>
2021-03-16 00:54:30 +0800

    * 第 1 题 两数之和 1. 暴力求解 O(n^2)

---
+++ src/com/eleven/Q1_两数之和/A1/Solution.java
@@ -0,0 +1,72 @@
+package com.eleven.Q1_两数之和.A1;
+
+/**
+ * 1. 两数之和
+ * 难度
+ * 简单
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ *
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ *
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ *
+ *
+ * 提示：
+ *
+ * 2 <= nums.length <= 103
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ * 
+ */
+
+
+import java.util.Arrays;
+
+/** 解法1
+ * 暴力求解 O(n^2)
+ * 枚举
+ */
+
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = {4, 3, 2, 1, 4};
+        int[] nums = {1,8,6,2,5};
+        
+        int[] a = twoSum(nums, 9);
+
+        System.out.println(Arrays.toString(a));
+    }
+}
